7af38282ed5fcfa3595f0458be25fb3c9991d44a
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-11-30 14:18:43 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,45 +9,25 @@
 #include <fstream>
 using namespace std;
 
-int countLetters(fstream fileName);
+void pressEnterToContinue(void);
 int main(void) {
-/*
-    int totalLetters = 0;
-    string fileName;
-    int letterAmounts[26];
-    double letterFrequencies[26];
-    
-    for(int i = 0; i < 26; i++) letterAmounts[i] = 0;
-    
-    cout << "Enter filename: ";
-    cin >> fileName;
-    ifstream fileStream(fileName);
-    
-    char ch;
-    fileStream.open(fileName);
-    if(!fileStream){
-        cout << "kys" << endl;
-        return 1;
-    }
-    
-    while(fileStream.get(ch) && !fileStream.eof()){
-        if(isalpha(ch)){
-            totalLetters++;
-        }
-        
-    }
-    cout << totalLetters;
-    */
- 
+
     while(true){
+        int letterAmounts[26];
+        double letterFrequencies[26];
         ifstream lab3;
         string word;
         string fileName;
         cout << "Enter filename: ";
         cin >> fileName;
         lab3.open(fileName);
-        int countletters=0,countnum=0,countpunc=0,countspace=0,words=0,line=0;
+        int countletters=0;
         char character,prevchar = 0;
+        
+        for(int i = 0; i < 26; i++){
+            letterAmounts[i] = 0;
+        }
+        
         if(!lab3)
         {
             cout << "Could not open file" << endl;
@@ -58,40 +38,39 @@ int main(void) {
             if(isalpha(character))
             {
                 countletters++;
-            }
-            if (isdigit(character))
-            {
-                countnum++;
-            }
-            if (ispunct(character))
-            {
-                countpunc++;
-                if (isalpha(prevchar))
-                {
-                    words++;
+                
+                for(int i = 0; i < 26; i++){
+                    if(character == 65 + i || character == 97 + i) letterAmounts[i] += 1;
                 }
+                
             }
-            if (isspace(character))
-            {
-                countspace++;
-                if (isalpha(prevchar))
-                {
-                    words++;
-                }
-            }
-            if(character=='\n')
-            {
-                line++;
-            }
+            
             prevchar = character;
         }
-        cout << "There are " << countletters << " letters." << endl;
-        cout << "There are " << countnum << " numbers." << endl;
-        cout << "There are " << countpunc << " punctuations." << endl;
-        cout << "There are " << countspace << " spaces." << endl;
-        cout << "There are " << words << " words." << endl;
-        cout << "There are " << line << " sentences." << endl;
+        
+        for(int i = 0; i < 26; i++){
+            letterFrequencies[i] = (double) letterAmounts[i] / countletters * 100;
+        }
+        
+        cout << "\nTotal number of letters: " << countletters << endl;
+        printf("Frequencies: \n");
+        
+        for(int i = 0; i < 26; i++){
+            printf("\'%c\': %f\n", i + 65, letterFrequencies[i]);
+        }
+            
+        pressEnterToContinue();
+        
+        
+        
         lab3.close();
     }
 }
 
+void pressEnterToContinue(void){
+    char c;
+    cout << "Press Enter to continue...";
+    cin.ignore(1024, '\n');
+    do{cin.get(c); }while(c != '\n' && c != EOF);
+    return;
+}
